ee64af64e341119c93210896af1d26f41baf2d5c
UnLuckyAki <aristocrat98@mail.ru>
2020-09-25 16:57:34 +1000

    Add files via upload

--- Exercise 2/main.py
+++ Exercise 2/main.py
@@ -1,19 +1,26 @@
-
-file = open('res/res_1.csv', 'r')
-category = file.readline().split(',')
+import os
+userdata = 0
+list_of_files = os.listdir("res")
 usercat = input('Введите категорию поиска:')
-if usercat in category:
-    count = category.index(usercat)
-    userdata = input('Введите искомое слово:')
-    for line in file:
-        data = line.split(',')
-        if userdata == data[count]:
-            print('время начала сессии в unixtime:', data[0])
-            print('время конца сессии в unixtime:', data[1])
-            print('длительность сессии:', data[2])
-            print('логин пользователя:', data[3])
-            print('мак адрес абонента:', data[4])
-            print('идентификатор сессии:', data[5])
-            print('ip провайдерского оборудования, через которое шла связь с пользователем:', data[6])
-            exit()
-print('Данные некорректны')
+for file in list_of_files:
+    file = open('res/' + file, 'r')
+    category = file.readline().split(',')
+    if usercat in category:
+        count = category.index(usercat)
+        if userdata == 0:
+            userdata = input('Введите искомое слово:')
+        for line in file:
+            data = line.split(',')
+            if userdata == data[count]:
+                print('время начала сессии в unixtime:', data[0])
+                print('время конца сессии в unixtime:', data[1])
+                print('длительность сессии:', data[2])
+                print('логин пользователя:', data[3])
+                print('мак адрес абонента:', data[4])
+                print('идентификатор сессии:', data[5])
+                print('ip провайдерского оборудования, через которое шла связь с пользователем:', data[6])
+                exit()
+    else:
+        print('Неверная категория')
+        exit()
+print('Данные некорректны или совпадений не найдено')
